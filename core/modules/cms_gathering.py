e40cf0b8b36091208a16507902d2eab4de91030f
graniet <graniet75@gmail.com>
2017-01-13 19:32:21 +0100

    CMS gathering module (wordpress,joomla,magento)

--- core/modules/cms_gathering.py
+++ core/modules/cms_gathering.py
@@ -4,16 +4,22 @@
 from colorama import Fore,Back,Style
 
 import os,sys
-import urllib
+import requests
 
 class module_element(object):
 
 	def __init__(self):
-		self.title = "Domain gathering : \n"
-		self.require = {"enterprise":[{"value":"","required":"yes"}],"function":[{"value":"test","required":"no"}]}
+		self.title = "CMS gathering : \n"
+		self.require = {"website":[{"value":"","required":"yes"}]}
 		self.export = []
 		self.export_file = ""
 		self.export_status = False
+		self.cms = {
+			"wordpress":['/wp-includes/','/wp-admin/'],
+			"magento":['/frontend/default/','/static/frontend/'],
+			"joomla":['/administrator/','/templates/']
+		}
+		self.status_code = [200,403,301,302]
 
 	def set_agv(self, argv):
 		self.argv = argv
@@ -85,6 +91,25 @@ class module_element(object):
 			self.main()
 
 	def main(self):
-		print "main"
-
-
+		start = 0
+		website = self.get_options('website')
+		if "http//" in website:
+			website = website.replace('http//','http://')
+		print "* Checking for " + Fore.BLUE + website + Style.RESET_ALL
+		if website[-1:] == "/":
+			website = website[:-1]
+		try:
+			requests.get(website)
+			print Fore.GREEN + "* url is stable" + Style.RESET_ALL
+			start = 1
+		except:
+			print Fore.RED + "* url schema not correct" + Style.RESET_ALL
+		if start == 1:
+			for line in self.cms:
+				print "* checking " + str(line)
+				for path in self.cms[line]:
+					complet_url = website + path
+					req = requests.get(complet_url)
+					if req.status_code in self.status_code:
+						print Fore.GREEN + "* possible using " + str(line) + " with : " + str(complet_url) + Style.RESET_ALL
+						self.export.append(complet_url + " ("+str(line)+")")
