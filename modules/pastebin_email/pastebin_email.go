42e2e43b8a942f8060bbff86f7166f0bf409650c
Tristan Granier <graniet75@gmail.com>
2019-07-28 12:35:59 +0200

    fix: pastebin_email

--- modules/pastebin_email/pastebin_email.go
+++ modules/pastebin_email/pastebin_email.go
@@ -69,8 +69,11 @@ func (module *PasteBinEmail) Start(){
 
 	paramLimit, _ := module.GetParameter("limit")
 	urlEnd := strings.Replace(target.GetName(), "@", "%40", -1)
-	url := "https://encrypted.google.com/search?num=" + paramLimit.Value + "&start=0&hl=en&q=site%3Apastebin.com%20\""+urlEnd+"\""
-	res, err := http.Get(url)
+	url := "https://www.google.com/search?num=" + paramLimit.Value + "&start=0&hl=en&q=site%3Apastebin.com%20\""+urlEnd+"\""
+	client := http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+	res, err := client.Do(req)
 	if err != nil {
 		module.Stream.Error("Argument 'URL' can't be reached.")
 		return
